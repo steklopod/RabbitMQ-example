01fecfc375fc50693ecc4ad404a3066fc4922e4d
Dima Koltovich <steklopod@gmail.com>
2018-11-29 11:02:27 +0300

    @JsonAutoDetect & lombok annotations added to data-classes.

--- src/main/java/ru/steklopod/rabbitmqexample/PracticalTipMessage.java
+++ src/main/java/ru/steklopod/rabbitmqexample/PracticalTipMessage.java
@@ -1,20 +1,15 @@
 package ru.steklopod.rabbitmqexample;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@JsonAutoDetect
+@AllArgsConstructor
 public class PracticalTipMessage {
-
     private final String text;
     private final int priority;
     private final boolean secret;
 
-    public PracticalTipMessage(@JsonProperty("text") final String text,
-                               @JsonProperty("priority") final int priority,
-                               @JsonProperty("secret") final boolean secret) {
-        this.text = text;
-        this.priority = priority;
-        this.secret = secret;
-    }
 }
--- src/main/java/ru/steklopod/rabbitmqexample/PracticalTipMessageSimplified.java
+++ src/main/java/ru/steklopod/rabbitmqexample/PracticalTipMessageSimplified.java
@@ -1,25 +1,16 @@
 package ru.steklopod.rabbitmqexample;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@JsonAutoDetect
+@AllArgsConstructor
 public class PracticalTipMessageSimplified {
-
     private final String text;
-
-    public PracticalTipMessageSimplified(@JsonProperty("text") final String text) {
-        this.text = text;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    @Override
-    public String toString() {
-        return "PracticalTipMessageSimplified{" +
-                "text='" + text + '\'' +
-                '}';
-    }
 }
+
+
